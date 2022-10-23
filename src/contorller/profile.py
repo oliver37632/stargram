f91c0c239896673388bdb4e053bfc0abb6685f9f
KimSeungJin <klli1naver@gmail.com>
2022-10-23 21:50:24 +0900

    update

--- src/contorller/profile.py
+++ src/contorller/profile.py
@@ -69,7 +69,7 @@ def delete_image(account_id):
 
 def modify_profile(name, introduce, link, image, account_id):
     with session_scope() as session:
-        user = session.query(ProfileTbl).filter(ProfileTbl.account_id == account_id).first()
+        user = session.query(ProfileTbl).filter(UserTbl.account_id == account_id, ProfileTbl.id == UserTbl.id).first()
 
         user.image = image
         user.account_id = account_id
