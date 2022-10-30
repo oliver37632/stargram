e0c3b9e79b472faf2f2e23688fceaaa2f7becdf8
KimSeungJin <klli1naver@gmail.com>
2022-10-30 22:17:25 +0900

    update

--- src/contorller/poto.py
+++ src/contorller/poto.py
@@ -1,22 +1,23 @@
 from src.model.S3 import s3_connection
 
 from src.config import AWS_S3_BUCKET_NAME
-
+import uuid
 s3 = s3_connection()
 
 def upload(file, acl="public-read"):
+    name = file.filename + str(uuid.uuid4())
     try:
         s3.upload_fileobj(
             file,
             AWS_S3_BUCKET_NAME,
-            file.filename,
+            name,
             ExtraArgs={
                 "ACL": acl,
                 "ContentType": file.content_type
             }
         )
 
-        return f"https://s3.ap-northeast-2.amazonaws.com/gram.stargram/{file.filename}"
+        return f"https://s3.ap-northeast-2.amazonaws.com/gram.stargram/{name}"
     except Exception as e:
 
         return {"errors": str(e)}
