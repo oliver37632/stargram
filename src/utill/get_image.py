38e23c79b5d4af448cba1f3af529288ed8ace1f4
KimSeungJin <klli1naver@gmail.com>
2022-12-27 09:26:26 +0900

    update get_image

--- src/utill/get_image.py
+++ src/utill/get_image.py
@@ -6,4 +6,4 @@ def get_url(feed_id: str):
     with session_scope() as session:
         url = session.query(PhotoTbl.url).filter(PhotoTbl.feed_id == feed_id).all()
 
-        return url
+        return str(url)
