9f23b1877e087088b8ec2b5d53b84a20c85bc99e
KimSeungJin <klli1naver@gmail.com>
2022-12-27 09:12:27 +0900

    create utill

---
+++ src/utill/get_image.py
@@ -0,0 +1,9 @@
+from src.model import session_scope
+from src.model.photo import PhotoTbl
+from src.model.feed import FeedTbl
+
+def get_url(feed_id: str):
+    with session_scope() as session:
+        url = session.qurey(PhotoTbl.url).filter(PhotoTbl.feed_id == feed_id).all()
+
+        return url
